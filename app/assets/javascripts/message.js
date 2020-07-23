ff80b53fd9902a3969e4ec11a9b540e59a7b9b65
Mitsusawa-5 <aigacyo1989@gmail.com>
2020-07-23 16:34:24 +0900

    非同期通信のブランチ　7.23

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,61 @@
+$(function(){
+  function buildHTML(message){
+    if(message.image){
+      let html = `<div class="message">
+                  <div class="message__name">
+                  ${message.user_name}
+                  <div class="message__name__date">
+                  ${message.created_at}
+                  </div>
+                  </div>
+                  <div class="message__comment">
+                  <div class="message__comment__text">
+                  ${message.content}
+                  </div>
+                  <img class="message__image" src="${message.image}">
+                  </div>
+                  </div>`
+      return html;
+    } else {
+      let html = `<div class="message">
+                  <div class="message__name">
+                  ${message.user_name}
+                  <div class="message__name__date">
+                  ${message.created_at}
+                  </div>
+                  </div>
+                  <div class="message__comment">
+                  <div class="message__comment__text">
+                  ${message.content}
+                  </div>
+                  
+                  </div>
+                  </div>`
+      return html;
+
+    };
+  }
+  $(".form").on("submit", function(e){
+    e.preventDefault()
+    let formData = new FormData(this);
+    let url = $(this).attr("action");
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $(".message-list").append(html);
+      $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
+      $('form')[0].reset();
+      $(".send").prop("disabled",false);
+    })
+    .fail(function(){
+      alert("メッセージ送信に失敗しました");
+    });
+  })
+});
