111d739000dc3f8e869ed891a6c5217b82abf82f
Mitsusawa-5 <aigacyo1989@gmail.com>
2020-07-24 11:16:22 +0900

    自動更新機能の実装　7.24

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -1,7 +1,8 @@
 $(function(){
+
   function buildHTML(message){
     if(message.image){
-      let html = `<div class="message">
+      let html = `<div class="message" data-message-id=${message.id}>
                   <div class="message__name">
                   ${message.user_name}
                   <div class="message__name__date">
@@ -17,7 +18,7 @@ $(function(){
                   </div>`
       return html;
     } else {
-      let html = `<div class="message">
+      let html = `<div class="message" data-message-id=${message.id}>
                   <div class="message__name">
                   ${message.user_name}
                   <div class="message__name__date">
@@ -59,4 +60,28 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   })
+
+  // let reloadMessages = function() {
+  //   let last_message_id = $('.MessageBox:last').data("message-id") || 0;
+  //   $.ajax({
+  //     url: "api/messages",
+  //     type: "GET",
+  //     dataType: "json",
+  //     data: {id: last_message_id}
+  //   })
+  //   .done(function(messages){
+  //     if (messages.length !== 0) {
+  //       let insertHTML = '';
+  //       $.each(messages, function(i, message) {
+  //         insertHTML += buildHTML(message)
+  //       });
+  //       $('.message-list').append(insertHTML);
+  //       $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
+  //     }
+  //   })
+  //   .fail(function(){
+  //     alert("error");
+  //   });
+  // };
+  // setInterval(reloadMessages, 7000);
 });
